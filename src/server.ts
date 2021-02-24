4b7eada28e5edc064aa8f5567313d94a00d94357
AdrianaScrobote <adriana_100000@gmail.com>
2021-02-23 22:39:20 -0300

    feat(users): init use database, migrations, controllers, repositories ...

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
+import "reflect-metadata"
 import express from "express"
-
+import './database'
+import {router} from "./routers"
 const app = express()
 
+app.use(express.json())
+app.use(router)
+
 app.listen(3333, () => console.log("server is running!"))
-
-
-app.get("/", (request, response) => {
-    return response.json({"Mensagem": "Hello World - NLW04"})
-})
-
-app.post('/', (request, response) => {
-    return response.json({"Mensagem": "Os dados foram salvos com sucesso!"})
-})
